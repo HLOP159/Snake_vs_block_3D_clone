ec4dd2f140638ea3d08495e15e944c15947f12e3
HLOP159 <kovalev159357@gmail.com>
2022-09-24 23:23:04 +0300

    with Finish Particle System

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class Finish : MonoBehaviour
 {
+    public ParticleSystem ParticleSystemLeft;
+    public ParticleSystem ParticleSystemRight;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            PlayParycleSystem();
             other.GetComponent<Player>().ReachFinish();
         }
+    }
+    public void PlayParycleSystem()
+    {
+        ParticleSystemLeft.Play();
+        ParticleSystemRight.Play();
     }
 }
